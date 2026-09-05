70cbdaf7ea6a79f5c6bdceb07b54cadb3b9c4cfe
Shivam Sagar <shivamkv3patialapunjab@gmail.com>
2026-09-05 14:43:58 +0530

    Add AND gate demonstration code in Arduino

---
+++ Logic_Gates_Demonstration/AND_gate.ino
@@ -0,0 +1,20 @@
+const int button1 = 18;
+const int button2 = 19;
+const int led = 21;
+
+void setup() {
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  bool A = !digitalRead(button1);
+  bool B = !digitalRead(button2);
+
+  if (A && B) {
+    digitalWrite(led, HIGH);
+  } else {
+    digitalWrite(led, LOW);
+  }
+}
