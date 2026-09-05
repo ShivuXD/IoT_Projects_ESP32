d8e842233817b4b5924483e01f7f94bdc12ec261
Shivam Sagar <shivamkv3patialapunjab@gmail.com>
2026-09-05 14:59:01 +0530

    Create NOR_gate.ino

---
+++ Logic_Gates_Demonstration/NOR_gate.ino
@@ -0,0 +1,18 @@
+const int button1 = 18;
+const int button2 = 19;
+const int led = 21;
+
+void setup() {
+  pinMode(button1, INPUT_PULLUP);
+  pinMode(button2, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  bool A = !digitalRead(button1);
+  bool B = !digitalRead(button2);
+
+  bool output = !(A || B);
+
+  digitalWrite(led, output);
+}
